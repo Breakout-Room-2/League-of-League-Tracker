e702d5e671ac5148f42c52863d788383924024ee
Akar Singh <Akar96684@gmail.com>
2021-02-13 23:50:17 -0800

    auxiliary script for getting api_key from site added

--- powershell_testing/get_dev_key.ps1
+++ powershell_testing/get_dev_key.ps1
@@ -1,71 +1,152 @@
-# File setup to avoid having to login each time
-# Login creds stored as securestring and converted when needed
-$datafile = ".\login.json"
-$success_msg = "Sucessfully logged in, writing API_key to 'dev_key' file now...";
-$found_msg = "`nlogin info file found, just sit back and let the script do its thing...";
-$notfound_msg = "`nNo login info file found, you're gonna have to login yourself:";
+# Script-wide definitions for datafile location, messages, and urls
+$newfile        = 1;
+$datafile       = ".\login.json";
+$found_msg      = "`nLogin info found, just sit back and let the script do it's thing";
+$notfound_msg   = "`nNo login file, you're gonna have to input some values yourself:";
+$saving_msg     = "`nLogin successful, writing login info to 'login.json' file...";
+$failure_msg    = "`nLogin failed, rerun script to enter credentials again...";
+$writing_msg    = "writing API key to 'dev_key' file...";
+$login_url      = "developer.riotgames.com/login";
+$auth_url       = "auth.riotgames.com/api/v1/authorization";
 
-if(Test-Path $datafile){
-    Write-Host $found_msg;
+#                       Start of Helper Functions
+# ----------------------------------------------------------------------------
+#
+# Helper function found online that will transform securestring --> plaintext
+function decryptSecureString($SecureString){
+    return [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($SecureString));
+}
 
-# Convert from json to PSCustomObject
+# Helper function to gather user credentials. Will try to use and store
+# login credentials as secure strings in a file to avoid having to retype
+# them each time. 
+function getUserCred(){
+# If no datafile found
+# Notify user
+# Prompt user for username and password
+# Store as securestrings in file
+# Then continue with the rest of the code
+    if (!(Test-Path $datafile)) {
+        Write-Host -ForegroundColor Magenta $notfound_msg;
+
+        $username = Read-Host -Prompt "Username" | ConvertTo-SecureString -AsPlainText;
+        $password = Read-Host -Prompt "Password" -AsSecureString;
+
+        (@{Username=$($username | ConvertFrom-SecureString);
+            Password=$($password | ConvertFrom-SecureString);
+            }) | ConvertTo-Json > $datafile;
+    }
+
+# If datafile was found, all above code can get skipped
+# Notify user and mark that newfile was not created
+    else {
+        Set-Variable -Name newfile -Value 0 -Scope Script;
+        Write-Host -ForegroundColor Cyan $found_msg;
+    }
+
+# Extract data from the json file 
+# Then return decrypted data
     $data = Get-Content $datafile | ConvertFrom-Json;
 
-# Extract login data from PSCustomObject and decrypt;
-    $username = $data.Username | ConvertTo-SecureString 
-        | ConvertFrom-SecureString -AsPlainText;
-    $password = $data.Password | ConvertTo-SecureString 
-        | ConvertFrom-SecureString -AsPlainText;
+    $data.username = decryptSecureString($data.username | ConvertTo-SecureString);
+    $data.password = decryptSecureString($data.password | ConvertTo-SecureString);
+
+    return $data;
 }
 
-# Prompt user for usernamd and password if not defined
-#   store into file to avoid re-prompting next time
-else{
-    Write-Host $notfound_msg;
+# Helper function to parse the dev page for apikey and expiration date,
+# encode as securestrings and write as json to a file
+# using Powershell V5
+function parseForKeyV5($dev_page){
+# Both key and date can be found from element w/ id "apikey"
+    $element = $dev_page.parsedHTML.getElementById("apikey");
+    $date = $element.parentElement.parentElement.textContent;
 
-    $username = Read-Host -Prompt "Username" 
-        | ConvertTo-SecureString -AsPlainText;
-    $password = Read-Host -Prompt "Password" -AsSecureString;
+# For a clean conversion to DateTime, run through a series of regex replaces
+# to remove text before/after date, the @, and any ordinal number suffixes
+    @('.*Expires: ', '\s\(.*', '@', 'th', 'rd', 'nd', 'st') | foreach {
+        $date = $date -replace $_;
+    }
 
-# While the data is still in securestring format, store 
-# as a json object into a file
-    (@{Username=$($username | ConvertFrom-SecureString);
-     Password=$($password | ConvertFrom-SecureString);}) 
-        | ConvertTo-Json > $datafile;
+# Get rid of extra space after converting to DateTime and 
+# encode api key as a secure string before writing to file
+    $date = Get-Date $date | sed -n '2p';
+    $key  = $element.value | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString;
 
-# Now convert the data back to plainstrings to use in script
-    $username = $username | ConvertFrom-SecureString -AsPlainText;
-    $password = $password | ConvertFrom-SecureString -AsPlainText;
+    @{api_key=$key; exp_date=$date} | ConvertTo-Json > dev_key;
 }
 
-# Define the url for login and authentication pages
-$login_url = "developer.riotgames.com/login";
-$auth_url = "auth.riotgames.com/api/v1/authorization";
+# Helper function to parse the dev page for apikey and expiration date,
+# encode as securestrings and write as json to a file
+# using Powershell V7
+function parseForKeyV7($dev_page){
+# Key can be found under inputFields w/ id "apikey"
+    $key = $dev_page.InputFields.findById("apikey").value;
 
+# Sadly could not find a way to convert htm source --> html object in pwsh 7
+# so resorted to regex to parse the response content for expiration date
+# Similar to V5, run through series of regex replacements to clean up for
+# DateTime conversion
+    $date = $dev_page.content | sed -n '/Expires/ {n;n;p}';
+    @('\s\(.*', '@', 'th', 'rd', 'nd', 'st') | foreach {
+        $date = $date -replace $_;
+    }
+
+# Get rid of extra space after converting to DateTime and 
+# encode api key as a secure string before writing to file
+    $date = Get-Date $date | sed -n '2p';
+    $key = ConvertTo-SecureString -AsPlainText $key | ConvertFrom-SecureString;
+
+    @{api_key=$key; exp_date=$date} | ConvertTo-Json > dev_key;
+}
+# ----------------------------------------------------------------------------
+#                   End of Helper Functions
+
+#                    Starting of main code
+# ----------------------------------------------------------------------------
+do{
+# Attempt to retrieve user data w/ helper function and 
 # Generate the payload for the upcoming PUT request as a json object
-$payload = (@{type='auth'; 
-    language='en_US'; remember='false';
-    username=$username;
-    password=$password; }) | ConvertTo-Json;
+    $data = getUserCred;
+    $payload = (@{type='auth';
+            language='en_US';
+            remember='false';
+            username=$($data.username);
+            password=$data.password; }) | ConvertTo-Json;
 
 # Get the login page taking all redirections and create a session variable
-$login_page = Invoke-WebRequest -Uri $login_url -SessionVariable session;
-
+    $login_page = Invoke-WebRequest -Uri $login_url -SessionVariable session;
 
 # Inspect element reveals the login process involves submitting a PUT request
 # with the payload (specifying content as application/json form) which is odd
-$oath_page = Invoke-WebRequest -Uri $auth_url -WebSession $session `
-    -Method PUT -Body $payload -ContentType 'application/json'
+    $oath_page = Invoke-WebRequest -Uri $auth_url -WebSession $session `
+        -Method PUT -Body $payload -ContentType 'application/json';
+    $oath_resp = $oath_page.Content | ConvertFrom-Json;
+
+# Repeat process until user succesfully logs in or interrupts script
+# additionally notify user if saving a new file
+if (!($oath_resp.error)){
+    if ($newfile){
+        Write-Host -ForegroundColor Green $saving_msg;
+    }
+    break;
+}
+
+Write-Host -ForegroundColor Yellow $failure_msg;
+Remove-Item $datafile;
+} while(1);
 
 # Dev url can be parsed from the contents of the auth response if successful
-$dev_url = ($oath_page.Content 
-        | ConvertFrom-Json).response.parameters.uri;
-
-# You can finally get the developer page using the now loaded
-# session variable
+# You can then get to the dev page w/ the cookies set in session variable
+$dev_url = $oath_resp.response.parameters.uri;
 $dev_page = Invoke-WebRequest -Uri $dev_url -WebSession $session; 
 
-Write-Host "`n$success_msg";
-
-# Recover the api key from the dev page by finding w/ id
-$dev_page.InputFields.findById("apikey").value | ConvertTo-SecureString -AsPlainText | ConvertFrom-SecureString > dev_key;
+# Depending on version of powershell, run the appropriate version of helper
+# and notify user of writing to file
+if((Get-Host).Version.Major -eq 5){
+    parseForKeyV5($dev_page);
+} else {
+    parseForKeyV7($dev_page);
+}
+Write-Host -ForegroundColor Green $writing_msg
+# ----------------------------------------------------------------------------
