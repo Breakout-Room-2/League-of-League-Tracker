d4f78d4834c9a170d9fa45a7d328bb1c029c9344
Akar Singh <aks002@ucsd.edu>
2021-02-23 09:42:51 -0800

    Moved summoner search logic from Main to Search Activity

--- app/src/main/java/com/example/leagueoflegendstracker/MainActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/MainActivity.java
@@ -1,56 +1,25 @@
 package com.example.leagueoflegendstracker;
 
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import com.codepath.asynchttpclient.AsyncHttpClient;
-import com.codepath.asynchttpclient.RequestParams;
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
-import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
-
-import okhttp3.Headers;
+import com.example.leagueoflegendstracker.models.Summoner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    public static final String API_KEY = BuildConfig.API_KEY;
 
     public static final String BASE_URL = "https://na1.api.riotgames.com%s";
     public static final String SUMMONER_ENDPOINT = "/lol/summoner/v4/summoners/by-name/%s";
     public static final String MATCH_LIST_ENDPOINT = "/lol/match/v4/matchlists/by-account/%s";
     public static final String MATCH_DETAIL_ENDPOINT = "/lol/match/v4/matches/%s";
     public static final String MASTERIES_ENDPOINT = "/lol/champion-mastery/v4/champion-masteries/by-summoner/%s";
-    public static final String API_KEY = BuildConfig.API_KEY;
-    public static final String SUMMONER = "Marethyu86";
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("api_key", API_KEY);
-
-        getSummonerInfo(SUMMONER, client, params);
-    }
-
-    public static void makeApiRequest(String endPoint, AsyncHttpClient client, RequestParams params){
-        Log.i(TAG, "Making call to " + String.format(BASE_URL, endPoint));
-        client.get(String.format(BASE_URL, endPoint), params, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.d(TAG, "Success, with data: "+json);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.d(TAG, "Failure, with message: "+response);
-            }
-        });
-    }
-
-    public static void getSummonerInfo(String summoner, AsyncHttpClient client, RequestParams params){
-        makeApiRequest(String.format(SUMMONER_ENDPOINT, summoner), client, params);
     }
 }
--- app/src/main/java/com/example/leagueoflegendstracker/SearchActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/SearchActivity.java
@@ -11,11 +11,26 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.leagueoflegendstracker.models.Summoner;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
+
 public class SearchActivity extends AppCompatActivity {
 
     public static final String TAG = "SearchActivity";
+    public static final String API_KEY = BuildConfig.API_KEY;
+
+    public static final String BASE_URL = "https://na1.api.riotgames.com%s";
+    public static final String SUMMONER_ENDPOINT = "/lol/summoner/v4/summoners/by-name/%s";
+
     EditText etSummonerSearch;
     ImageView ivSearchButton;
+    Summoner summoner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +44,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH){
-                    performSearch();
+                    performSearch(etSummonerSearch.getText().toString());
                     return true;
                 }
                 return false;
@@ -38,12 +53,35 @@ public class SearchActivity extends AppCompatActivity {
         ivSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                performSearch();
+                performSearch(etSummonerSearch.getText().toString());
             }
         });
     }
 
-    private void performSearch() {
+    private void performSearch(String summonerName) {
         Log.i(TAG, "User is searching for: "+etSummonerSearch.getText());
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("api_key", API_KEY);
+
+        String url = String.format(BASE_URL, String.format(SUMMONER_ENDPOINT, summonerName));
+        Log.i(TAG, "Making call to: " + url);
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "Success, with data: "+json);
+                try {
+                    summoner = new Summoner(json.jsonObject);
+                    Log.i(TAG, "Successfully created summoner model: "+summoner.toString());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit jsonException: "+e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "Failure, with resp: "+response);
+            }
+        });
     }
 }
