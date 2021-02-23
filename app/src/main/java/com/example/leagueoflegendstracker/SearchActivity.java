2223a9a9bb9be771aaf0d89893d4cf3198940e2a
Akar Singh <aks002@ucsd.edu>
2021-02-22 21:13:21 -0800

    User inputing 'Enter' on keyboard now also triggers a search

--- app/src/main/java/com/example/leagueoflegendstracker/SearchActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/SearchActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -22,11 +25,25 @@ public class SearchActivity extends AppCompatActivity {
         etSummonerSearch = findViewById(R.id.etSummonerSearch);
         ivSearchButton = findViewById(R.id.ivSearchButton);
 
+        etSummonerSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
         ivSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "User is searching for: "+etSummonerSearch.getText());
+                performSearch();
             }
         });
+    }
+
+    private void performSearch() {
+        Log.i(TAG, "User is searching for: "+etSummonerSearch.getText());
     }
 }
