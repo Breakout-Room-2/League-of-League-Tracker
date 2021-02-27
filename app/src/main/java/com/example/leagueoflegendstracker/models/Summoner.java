c8cb361599d82c5ac52224920362da144492d44c
Akar Singh <aks002@ucsd.edu>
2021-02-27 11:03:28 -0800

    Workaround for asynchronous network requests not filling models in time used, failed attempts kept for reference

--- app/src/main/java/com/example/leagueoflegendstracker/models/Summoner.java
+++ app/src/main/java/com/example/leagueoflegendstracker/models/Summoner.java
@@ -6,12 +6,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Summoner {
+    String summonerName;
     String encryptedSummonerId;
     String accountId;
     int profileIconId;
     int summonerLevel;
 
     public Summoner(JSONObject jsonObject) throws JSONException {
+        summonerName = jsonObject.getString("name");
         encryptedSummonerId = jsonObject.getString("id");
         accountId = jsonObject.getString("accountId");
         profileIconId = jsonObject.getInt("profileIconId");
@@ -34,4 +36,7 @@ public class Summoner {
         return summonerLevel;
     }
 
+    public String getSummonerName() {
+        return summonerName;
+    }
 }
