cfef32960c5fe6688aa4c19ea9d8eec0f3344e52
Akar Singh <Akar96684@gmail.com>
2021-03-17 16:43:19 -0700

    Cleaned up Adapter code

--- app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsAdapter.java
+++ app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsAdapter.java
@@ -10,65 +10,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.leagueoflegendstracker.models.MatchSummary;
 
-import java.util.HashMap;
 import java.util.List;
-
-//import static com.example.leagueoflegendstracker.IdConverter.CHAMP_DATA;
-//
-//  Summoner summoner;
-//          ArrayList<Mastery> top_masteries;
-//        ArrayList<League> leagues;
-//        ArrayList<MatchSummary> matchList;
-//
-//        ImageView ivChampOne, ivChampTwo, ivChampThree, ivSummonerIcon;
-//        TextView tvSummonerName, tvSummonerLevel, tvChampOne, tvChampTwo, tvChampThree, tvRank;
+import java.util.Locale;
 
 public class MatchDetailsAdapter extends RecyclerView.Adapter<MatchDetailsAdapter.ViewHolder> {
-    public static final String CHAMP_ICONS_ENDPOINT    = "https://cdn.communitydragon.org/latest/champion/%s/square";
+
     Context context;
     List<MatchSummary>  matches;
-    HashMap<Integer, String> champData;
+
     public MatchDetailsAdapter(Context context, List<MatchSummary> matches) {
-        champData = new HashMap<Integer, String>();
         this.context = context;
         this.matches = matches;
     }
-
-//    private void getChampData(){
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(CHAMP_DATA, new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Headers headers, JSON json) {
-//                Log.i(TAG, "Successful datadragon call to: " + CHAMP_DATA);
-//                // Log.i(TAG, "Success, with data: " + json);
-//                createChampData(json.jsonObject);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-//                Log.i(TAG, "Failure, with data: " + response);
-//            }
-//        });
-//    }
-//
-//    private void createChampData(JSONObject json) {
-//        try {
-//            champData = new HashMap<>();
-//            JSONObject data = json.getJSONObject("data");
-//
-//            for (Iterator<String> keys = data.keys(); keys.hasNext(); ) {
-//                JSONObject champ = data.getJSONObject(keys.next());
-//                int champID = Integer.parseInt(champ.getString("key"));
-//                String champName = champ.getString("name");
-//                champData.put(champID, champName);
-//            }
-//        } catch (JSONException e) {
-//            Log.i(TAG, "Ran into jsonException: " + e);
-//        }
-//    }
 
     @NonNull
     @Override
@@ -99,44 +54,39 @@ public class MatchDetailsAdapter extends RecyclerView.Adapter<MatchDetailsAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivChampionBox;
-        ImageView ivitem1, ivitem2, ivitem3, ivitem4, ivitem5, ivitem6;
+
+        private final String KDA = "%d/%d/%d";
+
+        ImageView ivChampionBox, ivitem1, ivitem2, ivitem3, ivitem4, ivitem5, ivitem6;
         TextView tvChampName, tvLevel, tvKDA, tvCS, tvDuration, tvMode;
-//        TextView
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ivChampionBox = itemView.findViewById(R.id.ivChampionBox);
-            ivitem1 = itemView.findViewById(R.id.ivItem1);
-            ivitem2 = itemView.findViewById(R.id.ivItem2);
-            ivitem3 = itemView.findViewById(R.id.ivItem3);
-            ivitem4 = itemView.findViewById(R.id.ivItem4);
-            ivitem5 = itemView.findViewById(R.id.ivItem5);
-            ivitem6 = itemView.findViewById(R.id.ivItem6);
-            tvChampName = itemView.findViewById(R.id.tvChampName); //SummonerName is Champion name
-            tvLevel = itemView.findViewById(R.id.tvLevel);
-            tvKDA = itemView.findViewById(R.id.tvKDA);
-            tvCS = itemView.findViewById(R.id.tvCS);
-            tvDuration = itemView.findViewById(R.id.tvDuration);
-            tvMode = itemView.findViewById(R.id.tvMode);
+            ivChampionBox   = itemView.findViewById(R.id.ivChampionBox);
+            ivitem1         = itemView.findViewById(R.id.ivItem1);
+            ivitem2         = itemView.findViewById(R.id.ivItem2);
+            ivitem3         = itemView.findViewById(R.id.ivItem3);
+            ivitem4         = itemView.findViewById(R.id.ivItem4);
+            ivitem5         = itemView.findViewById(R.id.ivItem5);
+            ivitem6         = itemView.findViewById(R.id.ivItem6);
+            tvChampName     = itemView.findViewById(R.id.tvChampName);
+            tvLevel         = itemView.findViewById(R.id.tvLevel);
+            tvKDA           = itemView.findViewById(R.id.tvKDA);
+            tvCS            = itemView.findViewById(R.id.tvCS);
+            tvDuration      = itemView.findViewById(R.id.tvDuration);
+            tvMode          = itemView.findViewById(R.id.tvMode);
         }
+
         public void bind(MatchSummary match) {
-            MatchSummary.Participant currentSumm = match.getUserParticipant();
-            tvChampName.setText(champData.get(match.getChampion()));
-            String champIcon = champData.get(match.getChampion());
-//            Glide.with(this).load(champIcon).into(ivChampionBox);
-            Glide.with(itemView).load(champIcon).into(ivChampionBox);
-            MatchSummary.Participant.Stats userStats = currentSumm.getStats();
-            int[] currItems = userStats.getItems();
-            String kills = String.valueOf(userStats.getKills());
-            String deaths = String.valueOf(userStats.getDeaths());
-            String assists = String.valueOf(userStats.getAssists());
-            tvKDA.setText(kills + "/" + deaths + "/" + assists);
-            tvCS.setText(userStats.getCS());
-            tvDuration.setText(toString().valueOf(match.getGameDuration()));
-
-
-
-
+            MatchSummary.Participant user = match.getUserParticipant();
+            IdConverter.loadChampName(context, tvChampName, match.getChampion());
+            IdConverter.loadChampIcon(context, ivChampionBox, match.getChampion());
+            MatchSummary.Participant.Stats userStats = user.getStats();
+            int[] userItems = userStats.getItems();
+            int kills    = userStats.getKills();
+            int deaths   = userStats.getDeaths();
+            int assists  = userStats.getAssists();
+            tvKDA.setText(String.format(Locale.US, KDA, kills, deaths, assists));
         }
     }
 }
