558320b951bac42c6db24ea76d17ecd3fb9573c6
Akar Singh <aks002@ucsd.edu>
2021-03-17 21:24:51 -0700

    Base code for MatchDetail started

--- app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
@@ -1,4 +1,42 @@
 package com.example.leagueoflegendstracker;
 
-public class MatchDetailsActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MatchDetailsActivity extends Activity {
+
+    public static final String TAG  = "MatchDetailsActivity";
+
+    ImageView ivChampionBox, ivSpell1, ivSpell2, ivRune, ivItem1, ivItem2, ivItem3, ivItem4, ivItem5, ivItem6, ivItem7;
+    TextView tvLevel, tvSummoner, tvSummonerRank, tvKDA, tvCS, tvP, tvK, tvC, tvGold;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_details);
+
+        ivChampionBox   = findViewById(R.id.ivChampionBox);
+        ivSpell1        = findViewById(R.id.ivSpell1);
+        ivSpell2        = findViewById(R.id.ivSpell2);
+        ivRune          = findViewById(R.id.ivRune);
+        tvLevel         = findViewById(R.id.tvLevel);
+        tvSummoner      = findViewById(R.id.tvSummonerName);
+        tvSummonerRank  = findViewById(R.id.tvSummonerRank);
+        tvKDA           = findViewById(R.id.tvKDA);
+        tvCS            = findViewById(R.id.tvCS);
+        tvP             = findViewById(R.id.tvP);
+        tvK             = findViewById(R.id.tvK);
+        tvC             = findViewById(R.id.tvC);
+        tvGold          = findViewById(R.id.tvGold);
+        ivItem1         = findViewById(R.id.ivItem1);
+        ivItem2         = findViewById(R.id.ivItem2);
+        ivItem3         = findViewById(R.id.ivItem3);
+        ivItem4         = findViewById(R.id.ivItem4);
+        ivItem5         = findViewById(R.id.ivItem5);
+        ivItem6         = findViewById(R.id.ivItem6);
+        ivItem7         = findViewById(R.id.ivItem7);
+    }
 }
