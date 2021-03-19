1895306f8c9fe4cdd9f95da373f0399378d4aaa0
Akar Singh <aks002@ucsd.edu>
2021-03-18 22:49:43 -0700

    Found out Activity had 2 recycler views that needed adapters, renamed RVs in layout for clarity

--- app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
@@ -1,42 +1,32 @@
 package com.example.leagueoflegendstracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.util.Locale;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.leagueoflegendstracker.models.MatchSummary;
+
+import org.parceler.Parcels;
 
 public class MatchDetailsActivity extends Activity {
 
     public static final String TAG  = "MatchDetailsActivity";
 
-    ImageView ivChampionBox, ivSpell1, ivSpell2, ivRune, ivItem1, ivItem2, ivItem3, ivItem4, ivItem5, ivItem6, ivItem7;
-    TextView tvLevel, tvSummoner, tvSummonerRank, tvKDA, tvCS, tvP, tvK, tvC, tvGold;
+    MatchSummary matchSummary;
+    RecyclerView rvWinningTeam, rvLosingTeam;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_details);
+        matchSummary = (MatchSummary) Parcels.unwrap(getIntent().getParcelableExtra("match"));
 
-        ivChampionBox   = findViewById(R.id.ivChampionBox);
-        ivSpell1        = findViewById(R.id.ivSpell1);
-        ivSpell2        = findViewById(R.id.ivSpell2);
-        ivRune          = findViewById(R.id.ivRune);
-        tvLevel         = findViewById(R.id.tvLevel);
-        tvSummoner      = findViewById(R.id.tvSummonerName);
-        tvSummonerRank  = findViewById(R.id.tvSummonerRank);
-        tvKDA           = findViewById(R.id.tvKDA);
-        tvCS            = findViewById(R.id.tvCS);
-        tvP             = findViewById(R.id.tvP);
-        tvK             = findViewById(R.id.tvK);
-        tvC             = findViewById(R.id.tvC);
-        tvGold          = findViewById(R.id.tvGold);
-        ivItem1         = findViewById(R.id.ivItem1);
-        ivItem2         = findViewById(R.id.ivItem2);
-        ivItem3         = findViewById(R.id.ivItem3);
-        ivItem4         = findViewById(R.id.ivItem4);
-        ivItem5         = findViewById(R.id.ivItem5);
-        ivItem6         = findViewById(R.id.ivItem6);
-        ivItem7         = findViewById(R.id.ivItem7);
+        rvWinningTeam   = findViewById(R.id.rvWinningTeam);
+        rvLosingTeam    = findViewById(R.id.rvLosingTeam);
+
+        // Participant[] to pass into adapter constructor once it's set up
+        // matchSummary.getWinningTeam();
+        // matchSummary.getLosingTeam();
     }
 }
