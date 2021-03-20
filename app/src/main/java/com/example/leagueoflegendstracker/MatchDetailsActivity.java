374c02cea596ed9de017edbb85ea5aa3adb1d02f
Akar Singh <aks002@ucsd.edu>
2021-03-19 19:24:23 -0700

    adapters for winning and losing team created and set to RV for winning/losing teams

--- app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
+++ app/src/main/java/com/example/leagueoflegendstracker/MatchDetailsActivity.java
@@ -1,14 +1,16 @@
 package com.example.leagueoflegendstracker;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.leagueoflegendstracker.models.MatchSummary;
 
 import org.parceler.Parcels;
+
+import java.util.Arrays;
 
 public class MatchDetailsActivity extends Activity {
 
@@ -16,6 +18,7 @@ public class MatchDetailsActivity extends Activity {
 
     MatchSummary matchSummary;
     RecyclerView rvWinningTeam, rvLosingTeam;
+    GameDetailsAdapter winTeamAdapter, loseTeamAdapter;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -25,8 +28,13 @@ public class MatchDetailsActivity extends Activity {
         rvWinningTeam   = findViewById(R.id.rvWinningTeam);
         rvLosingTeam    = findViewById(R.id.rvLosingTeam);
 
-        // Participant[] to pass into adapter constructor once it's set up
-        // matchSummary.getWinningTeam();
-        // matchSummary.getLosingTeam();
+        winTeamAdapter  = new GameDetailsAdapter(this, Arrays.asList(matchSummary.getWinningTeam()));
+        loseTeamAdapter = new GameDetailsAdapter(this, Arrays.asList(matchSummary.getLosingTeam()));
+
+        rvWinningTeam.setLayoutManager(new LinearLayoutManager(this));
+        rvLosingTeam.setLayoutManager(new LinearLayoutManager(this));
+
+        rvWinningTeam.setAdapter(winTeamAdapter);
+        rvLosingTeam.setAdapter(loseTeamAdapter);
     }
 }
